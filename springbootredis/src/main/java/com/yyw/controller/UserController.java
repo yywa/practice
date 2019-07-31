f0a8a1918005c0c12c228aa736ef3a54be87d8d2
yuanyawei-pbj <yuanyawei-pbj@sinosig.com>
2019-07-31 16:31:04 +0800

    springboot整合swagger

--- springbootredis/src/main/java/com/yyw/controller/UserController.java
+++ springbootredis/src/main/java/com/yyw/controller/UserController.java
@@ -2,12 +2,15 @@ package com.yyw.controller;
 
 import com.yyw.entity.User;
 import com.yyw.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 /**
  * @author yyw
@@ -15,12 +18,15 @@ import java.util.Map;
  **/
 @RestController
 @RequestMapping("/user")
+@Api(value = "user查询相关接口")
 public class UserController {
     @Autowired
     private UserService service;
 
     @RequestMapping("/findUserById")
-    public User findUserById(@RequestParam int id) {
+    @ApiOperation(value = "查询用户信息", notes = "查询用户信息")
+    @ApiImplicitParam(name = "id", value = "id", required = true, dataType = "int")
+    public User findUserById(@RequestParam("id") int id) {
         return service.findUserById(id);
     }
 }
