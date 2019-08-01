ea400d5731d38f88746f467c5824ef450c1c992c
yuanyawei-pbj <yuanyawei-pbj@sinosig.com>
2019-08-01 14:43:00 +0800

    819

--- leetcode/src/main/java/leetcode819/SolutionOne.java
+++ leetcode/src/main/java/leetcode819/SolutionOne.java
@@ -5,12 +5,14 @@ import java.util.*;
 /**
  * @author yyw
  * @date 2019/8/1
- *
+ * <p>
  * 执行用时 :26 ms, 在所有 Java 提交中击败了78.78%的用户
  * 内存消耗 :37.5 MB, 在所有 Java 提交中击败了82.89%的用户
+ * <p>
+ * 解题思路：一个字符一个字符判断，然后切割成需要判断的各个字符串，然后利用map记录每个字符串的出现次数，用一个标志位和标志字符串来记录出现次数最多的。
  **/
 public class SolutionOne {
-    public static String mostCommonWord(String paragraph, String[] banned) {
+    private static String mostCommonWord(String paragraph, String[] banned) {
         int max = 1;
         String result = null;
         Map<String, Integer> map = new HashMap<>();
