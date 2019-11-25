428419a654cf5931e004589e77e237a44078b2e3
yywa <18435185727@163.com>
2019-11-25 23:18:17 +0800

    ribbon负载均衡

---
+++ springcloud/microservice-simple-consumer-movie-ribbon/src/main/java/study/user/controller/MovieController.java
@@ -0,0 +1,32 @@
+package study.user.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import study.user.entity.User;
+
+@RestController
+public class MovieController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.restTemplate.getForObject("http://microservice-provider-user/" + id, User.class);
+    }
+
+    @GetMapping("/log-user-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        MovieController.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+    }
+}
