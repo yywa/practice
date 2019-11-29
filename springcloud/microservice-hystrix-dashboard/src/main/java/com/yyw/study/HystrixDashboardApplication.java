094f01a8c94daa0876422038736f8585d6fe8f05
yywa <18435185727@163.com>
2019-11-29 21:03:51 +0800

    hystrix监控

---
+++ springcloud/microservice-hystrix-dashboard/src/main/java/com/yyw/study/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.yyw.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author yyw
+ * @date 2019/11/29
+ */
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
