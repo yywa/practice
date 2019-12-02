138d23841418e0452101855dc678581d301c22f5
yywa <18435185727@163.com>
2019-12-02 22:11:22 +0800

    文件上传下载。

---
+++ springcloud/microservicec-file-upload/src/main/java/com/yyw/study/FileApplication.java
@@ -0,0 +1,17 @@
+package com.yyw.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yyw
+ * @date 2019/12/2
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class FileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+}
